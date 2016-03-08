ad2ad3c64222045639b70701af10a6c1d50232ee
edilsonjustiniano <edilson.souza@inatel.br>
2016-03-08 09:25:23 -0300

    Initial Structure

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name financeAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the financeAppApp
+ */
+angular.module('financeAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
